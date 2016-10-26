1c53b1bb2613f0df23047ef088cdd52fa193d006
David Pribula <davidpribula@gmail.com>
2016-10-26 22:49:35 +0200

    Created Song entity

--- MusicLibrary/src/main/java/cz/muni/fi/pa165/entity/Song.java
+++ MusicLibrary/src/main/java/cz/muni/fi/pa165/entity/Song.java
@@ -1,9 +1,10 @@
 package cz.muni.fi.pa165.entity;
 
+import java.util.Objects;
 import javax.persistence.*;
 
 /**
- * Created by olda on 26.10.2016.
+ * Created David Pribula
  */
 @Entity
 @Table(name = "SONG")
@@ -13,13 +14,115 @@ public class Song {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(updatable = false, nullable = false, name = "id")
     private Long id;
-
-
+    
+    @Column(nullable=false, length = 64)
+    private String title;
+    
+    @ManyToOne
+    private Album album;
+    
+    @Column(nullable = false)
+    private int position;
+    
+    @ManyToOne
+    private Genre genre;
+    
+    private int bitrate;
+        
+    @Column(length = 2048)
+    private String commentary;
+        
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public int getBitrate() {
+        return bitrate;
+    }
+
+    public void setBitrate(int bitrate) {
+        this.bitrate = bitrate;
+    }
+
+    public String getCommentary() {
+        return commentary;
+    }
+
+    public void setCommentary(String commentary) {
+        this.commentary = commentary;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 23 * hash + Objects.hashCode(this.title);
+        hash = 23 * hash + Objects.hashCode(this.album);
+        hash = 23 * hash + this.position;
+        hash = 23 * hash + Objects.hashCode(this.genre);
+        hash = 23 * hash + this.bitrate;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Song other = (Song) obj;
+        if (this.position != other.position) {
+            return false;
+        }
+        if (this.bitrate != other.bitrate) {
+            return false;
+        }
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        if (!Objects.equals(this.album, other.album)) {
+            return false;
+        }
+        return Objects.equals(this.genre, other.genre);
     }
 }
