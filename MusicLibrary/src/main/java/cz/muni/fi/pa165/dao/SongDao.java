38cf8631d87fbf484bbc0bffb11fd9bd172b051f
David Pribula <davidpribula@gmail.com>
2016-10-26 22:49:52 +0200

    Created SongDao

--- MusicLibrary/src/main/java/cz/muni/fi/pa165/dao/SongDao.java
+++ MusicLibrary/src/main/java/cz/muni/fi/pa165/dao/SongDao.java
@@ -1,9 +1,21 @@
 package cz.muni.fi.pa165.dao;
 
+import cz.muni.fi.pa165.entity.Song;
+import java.util.List;
+
 /**
  *
  * @author David Pribula
  */
-public class SongDao {
+public interface SongDao {
 
+    void createSong(Song song);
+    
+    void updateSong(Song song);
+    
+    void deleteSong(Song song);
+    
+    void findSongById(Long id);
+    
+    List<Song> getAllSongs();
 }
