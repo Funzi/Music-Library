27c64287df884d7f267fc5067e9a9114744e197f
Jan Stourac <stourac.jan@gmail.com>
2016-10-30 18:53:27 +0100

    updated musician entity & added docs

--- MusicLibrary/src/main/java/cz/muni/fi/pa165/dao/MusicianDao.java
+++ MusicLibrary/src/main/java/cz/muni/fi/pa165/dao/MusicianDao.java
@@ -4,18 +4,46 @@ import cz.muni.fi.pa165.entity.Musician;
 import java.util.List;
 
 /**
+ * Data access object layer interface for Musician entity.
  *
  * @author Jan Stourac
  */
 public interface MusicianDao {
 
+	/**
+	 * Returns musician with given id or null.
+	 *
+	 * @param id musician id
+	 * @return musician with given id
+	 */
 	public Musician findById(Long id);
 
+	/**
+	 * Stores given musician in database.
+	 *
+	 * @param musician musician to store
+	 */
 	public void create(Musician musician);
 
+	/**
+	 * Updates given musician in database.
+	 *
+	 * @param musician musician to update
+	 * @return updated instance
+	 */
 	public Musician update(Musician musician);
 
+	/**
+	 * Deletes given musician from database.
+	 *
+	 * @param musician musician to delete
+	 */
 	public void delete(Musician musician);
 
+	/**
+	 * Returns all musicians from database.
+	 *
+	 * @return list with all musicians
+	 */
 	public List<Musician> findAll();
 }
--- MusicLibrary/src/main/java/cz/muni/fi/pa165/entity/Musician.java
+++ MusicLibrary/src/main/java/cz/muni/fi/pa165/entity/Musician.java
@@ -1,26 +1,38 @@
 package cz.muni.fi.pa165.entity;
 
+import cz.muni.fi.pa165.utils.Constants;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 /**
+ * Class representing musician entity.
  *
  * @author Jan Stourac
  */
 @Entity
+@Table(name = "MUSICIAN")
 public class Musician {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(updatable = false, nullable = false, name = "id")
 	private Long id;
 
 	@NotNull
-	@Column(nullable = false, unique = true)
+	@Column(nullable = false, unique = true, length = Constants.INT_LENGTH_SMALL)
 	private String name;
+
+	@ManyToMany
+	private Set<Album> albums = new HashSet<>();
 
 	public Musician(Long musicianId) {
 		this.id = musicianId;
@@ -29,24 +41,58 @@ public class Musician {
 	public Musician() {
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
+	/**
+	 * Returns unique identifier of musician. <strong>Warning:</strong> relevant
+	 * identifier is provided only when the object is persisted!
+	 *
+	 * @return unique identifier
+	 */
 	public Long getId() {
 		return id;
 	}
 
+	/**
+	 * Returns musician name.
+	 *
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Sets musician name. Maximal name length is limited to
+	 * {@link Constants#INT_LENGTH_SMALL}.
+	 *
+	 * @param name name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Adds album associated to this musician.
+	 *
+	 * @param album album
+	 */
+	public void addAlbum(Album album) {
+		this.albums.add(album);
+	}
+
+	/**
+	 * Returns all albums associated to this musician.
+	 *
+	 * @return albums
+	 */
+	public Set<Album> getAlbums() {
+		return albums;
+	}
+
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
+		int hash = 1;
+		hash = 13 * hash + Objects.hashCode(this.name);
+		return hash;
 	}
 
 	@Override
@@ -69,5 +115,10 @@ public class Musician {
 			return false;
 		}
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Musician{" + "id=" + id + ", name=" + name + ", albums=" + albums + '}';
 	}
 }
