b7aaa3ab25bd0f6599b3c93b2a89b873dab4ba45
Jan Stourac <stourac.jan@gmail.com>
2016-10-30 22:31:14 +0100

    added null tests to SongDaoTest

--- MusicLibrary/src/test/java/cz/muni/fi/pa165/dao/SongDaoTest.java
+++ MusicLibrary/src/test/java/cz/muni/fi/pa165/dao/SongDaoTest.java
@@ -61,9 +61,19 @@ public class SongDaoTest extends AbstractTestNGSpringContextTests {
 		songDao.create(song);
 	}
 
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void createNullTest() {
+		songDao.create(null);
+	}
+
 	@Test
 	public void findNoneTest() {
 		assertNull(songDao.findById(10L));
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void findNullTest() {
+		songDao.findById(null);
 	}
 
 	@Test
@@ -85,6 +95,11 @@ public class SongDaoTest extends AbstractTestNGSpringContextTests {
 
 		assertEquals(songDao.findById(song.getId()), song);
 		assertEquals(songDao.findById(song2.getId()), song2);
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void findAllNullTest() {
+		songDao.findById(null);
 	}
 
 	@Test
@@ -123,6 +138,11 @@ public class SongDaoTest extends AbstractTestNGSpringContextTests {
 		songDao.create(song2);
 		songDao.delete(song);
 		assertEquals(songDao.findById(song2.getId()), song2);
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void deleteNullTest() {
+		songDao.delete(null);
 	}
 
 	@Test
@@ -165,6 +185,11 @@ public class SongDaoTest extends AbstractTestNGSpringContextTests {
 		song.setTitle("random");
 		songDao.update(song);
 		assertEquals(songDao.findById(song2.getId()).getTitle(), song2.getTitle());
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void updateNullTest() {
+		songDao.update(null);
 	}
 
 	@AfterMethod
