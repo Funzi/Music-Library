ef5578201457599979f54cb01d4fbd7e97b420c1
Jan Stourac <stourac.jan@gmail.com>
2016-10-30 21:59:11 +0100

    equals and hashCode in Musician generated by NetBeans (to achieve same style)

--- MusicLibrary/src/main/java/cz/muni/fi/pa165/entity/Musician.java
+++ MusicLibrary/src/main/java/cz/muni/fi/pa165/entity/Musician.java
@@ -90,8 +90,8 @@ public class Musician {
 
 	@Override
 	public int hashCode() {
-		int hash = 1;
-		hash = 13 * hash + Objects.hashCode(this.name);
+		int hash = 5;
+		hash = 29 * hash + Objects.hashCode(this.name);
 		return hash;
 	}
 
@@ -100,17 +100,17 @@ public class Musician {
 		if (this == obj) {
 			return true;
 		}
-
 		if (obj == null) {
 			return false;
 		}
-
-		if (!(obj instanceof Musician)) {
+		if (getClass() != obj.getClass()) {
 			return false;
 		}
-
 		final Musician other = (Musician) obj;
-		return Objects.equals(name, other.name);
+		if (!Objects.equals(this.name, other.name)) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
