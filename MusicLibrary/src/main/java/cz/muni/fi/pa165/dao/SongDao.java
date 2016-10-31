893cd67636e1605c744c96d42dc85b77ba24e371
David Pribula <davidpribula@gmail.com>
2016-10-31 19:26:57 +0100

    javadoc for SongDao

--- MusicLibrary/src/main/java/cz/muni/fi/pa165/dao/SongDao.java
+++ MusicLibrary/src/main/java/cz/muni/fi/pa165/dao/SongDao.java
@@ -4,18 +4,45 @@ import cz.muni.fi.pa165.entity.Song;
 import java.util.List;
 
 /**
- *
+ * Data access object layer interface for Song entity.
  * @author David Pribula
  */
 public interface SongDao {
-
+    
+    /**
+     * Stores given song in database.
+     *
+     * @param song song to store
+     */
     void create(Song song);
 
+    /**
+     * Updates given song in database.
+     *
+     * @param song song to update
+     * @return song updated instance
+     */
     Song update(Song song);
 
+    /**
+     * Deletes given song from database.
+     *
+     * @param song song to delete
+     */
     void delete(Song song);
 
+    /**
+     * Returns song with given id or null.
+     *
+     * @param id song id
+     * @return song with given id
+     */
     Song findById(Long id);
 
+    /**
+     * Returns all songs from database.
+     *
+     * @return list containing all songs
+     */
     List<Song> findAll();
 }
