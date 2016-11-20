7a7df6ee06530bb40c935937d7ebbae6f877ae78
David Pribula <davidpribula@gmail.com>
2016-11-20 15:05:35 +0100

    GenreDTO - adding functionality

--- MusicLibrary-api/src/main/java/cz/muni/fi/pa165/api/dto/GenreDTO.java
+++ MusicLibrary-api/src/main/java/cz/muni/fi/pa165/api/dto/GenreDTO.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.api.dto;
 
 /**
+ * An DTO class representing a music genre. Genre has its id, name and a short
+ * description.
  *
- * @author david
+ * @author David Pribula
  */
 public class GenreDTO {
-    
+
+    private Long id;
+    private String name;
+    private String description;
+
+    public GenreDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
