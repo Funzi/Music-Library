98c981564fa5e00b39224f0b6a7471f39f8e5fab
Jan Stourac <stourac.jan@gmail.com>
2016-11-25 15:20:44 +0100

    improved handling of null objects in bean mapping service

--- MusicLibrary-service/src/main/java/cz/muni/fi/pa165/service/BeanMappingServiceImpl.java
+++ MusicLibrary-service/src/main/java/cz/muni/fi/pa165/service/BeanMappingServiceImpl.java
@@ -1,36 +1,33 @@
 package cz.muni.fi.pa165.service;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BeanMappingServiceImpl implements BeanMappingService{
+public class BeanMappingServiceImpl implements BeanMappingService {
 
-    @Autowired
-    private Mapper dozer;
+	@Autowired
+	private Mapper dozer;
 
-    @Override
-    public <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass) {
-        List<T> mappedCollection = new ArrayList<>();
-        for (Object object : objects) {
-            mappedCollection.add(dozer.map(object, mapToClass));
-        }
-        return mappedCollection;
-    }
+	@Override
+	public <T> List<T> mapTo(Collection<?> objects, Class<T> mapToClass) {
+		return objects != null
+				? objects.stream().map(u -> mapTo(u, mapToClass)).collect(Collectors.toList())
+				: null;
+	}
 
-    @Override
-    public  <T> T mapTo(Object u, Class<T> mapToClass)
-    {
-        return dozer.map(u,mapToClass);
-    }
+	@Override
+	public <T> T mapTo(Object u, Class<T> mapToClass) {
+		return u != null ? dozer.map(u, mapToClass) : null;
+	}
 
-    @Override
-    public Mapper getMapper(){
-    	return dozer;
-    }
+	@Override
+	public Mapper getMapper() {
+		return dozer;
+	}
 
 }
