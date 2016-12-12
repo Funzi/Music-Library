f922715ff0c5f01ab1c3601ec369f72c689247ac
olda <oldik.kon@gmail.com>
2016-12-13 00:35:44 +0100

    add methods to SongController and list songs

--- MusicLibrary-mvc/src/main/java/cz/muni/fi/pa165/mvc/controllers/SongController.java
+++ MusicLibrary-mvc/src/main/java/cz/muni/fi/pa165/mvc/controllers/SongController.java
@@ -1,16 +1,25 @@
 package cz.muni.fi.pa165.mvc.controllers;
 
+import cz.muni.fi.pa165.api.AlbumFacade;
+import cz.muni.fi.pa165.api.MusicianFacade;
 import cz.muni.fi.pa165.api.SongFacade;
+import cz.muni.fi.pa165.api.dto.MusicianDTO;
 import cz.muni.fi.pa165.api.dto.SongDTO;
 import cz.muni.fi.pa165.mvc.Alert;
 import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  *
@@ -20,8 +29,18 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/songs")
 public class SongController {
 
+	final static Logger log = LoggerFactory.getLogger(SongController.class);
+
+	public static final String REDIRECT_SONGS = "redirect:/songs/";
+
 	@Autowired
 	private SongFacade songFacade;
+
+	@Autowired
+	private MusicianFacade musicianFacade;
+
+	@Autowired
+	private AlbumFacade albumFacade;
 
 	@RequestMapping(value = "/update-position", method = RequestMethod.POST)
 	@PreAuthorize("hasAuthority('admin')")
@@ -44,4 +63,101 @@ public class SongController {
 
 		return "redirect:/albums/" + songDTO.getAlbum().getId();
 	}
+
+	@RequestMapping(value = "/")
+	public String list(Model model) {
+		List<SongDTO> songDTOList = songFacade.getAllSongs();
+
+		model.addAttribute("songDTOList",songDTOList);
+
+
+		return "songs/list";
+	}
+
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public String detail(@PathVariable Long id, Model model, RedirectAttributes redir) {
+		SongDTO songDTO = songFacade.getSongById(id);
+
+		if (songDTO == null) {
+			redir.addFlashAttribute(Alert.ERROR, "Song with #id=" + id +" not found in database");
+			return REDIRECT_SONGS;
+		} else {
+			model.addAttribute("songDTO", songDTO);
+		}
+
+		return "songs/detail";
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String add(Model model) {
+		model.addAttribute("songForm", new SongDTO());
+		return "songs/add";
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String doAdd(@ModelAttribute("songForm") SongDTO songDTO, RedirectAttributes redirect) {
+		Long id = songFacade.createSong(songDTO);
+		redirect.addFlashAttribute(Alert.SUCCESS, "Song Successfully created #id=" + id);
+
+		return REDIRECT_SONGS;
+	}
+
+	@RequestMapping(value = "/list/musician/{id}", method = RequestMethod.GET)
+	public String listForMusician(@PathVariable Long id, Model model, RedirectAttributes redir) {
+		MusicianDTO musicianDTO = musicianFacade.getMusicianById(id);
+		if (musicianDTO == null) {
+			redir.addFlashAttribute(Alert.ERROR, "Musician with #id=" + id + " not found in database");
+			return REDIRECT_SONGS;
+		}
+
+		List<SongDTO> songDTOList = songFacade.getSongsForMusician(musicianDTO);
+		model.addAttribute("songDTOList", songDTOList);
+		return "songs/listForMusician";
+	}
+
+	@RequestMapping(value = "/addSongToAlbum/{songId}/{albumId}", method = RequestMethod.POST)
+	public String addSongToAlbum(@PathVariable Long songId, @PathVariable Long albumId, RedirectAttributes redir) {
+		if (songFacade.getSongById(songId) == null) {
+			redir.addFlashAttribute(Alert.ERROR, "Song with #id=" + songId + " not found in database");
+			return REDIRECT_SONGS;
+		}
+		if (albumFacade.getAlbumById(albumId) == null) {
+			redir.addFlashAttribute(Alert.ERROR, "Album with #id=" + albumId + " not found in database");
+			return REDIRECT_SONGS;
+		}
+
+		try {
+			songFacade.assignSongToAlbum(songId, albumId);
+		}catch (Exception ex) {
+			redir.addFlashAttribute(Alert.ERROR, "Cannot assign song #id=" + songId+ " to album #id=" + albumId);
+			return REDIRECT_SONGS;
+		}
+		redir.addFlashAttribute(Alert.SUCCESS, "Song with #id=" + songId + " has been assigned to album #id=" + albumId);
+		return REDIRECT_SONGS;
+	}
+
+	@RequestMapping(value = "/{id}/delete", method = RequestMethod.GET)
+	public String delete(@PathVariable Long id, RedirectAttributes redir) {
+		if (songFacade.getSongById(id) == null) {
+			redir.addFlashAttribute(Alert.ERROR, "Song with #id=" + id + " not found in database");
+			return REDIRECT_SONGS;
+		}
+
+		try {
+			songFacade.deleteSong(id);
+		}catch (Exception ex) {
+			redir.addFlashAttribute(Alert.ERROR, "Cannot delete Song with #id=" + id);
+			return REDIRECT_SONGS;
+		}
+		redir.addFlashAttribute(Alert.SUCCESS, "Song with #id=" + id + " has been deleted");
+		return REDIRECT_SONGS;
+	}
+
+
+
+
+
+
+
 }
---
+++ MusicLibrary-mvc/src/main/webapp/WEB-INF/jsp/songs/list.jsp
@@ -0,0 +1,35 @@
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="utf-8" trimDirectiveWhitespaces="true" session="false" %>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="my" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+
+
+<my:pagetemplate title="Songs">
+    <jsp:attribute name="body">
+
+        <table class="table table-striped">
+            <tr>
+                <th>Title</th>
+                <th>Musician</th>
+                <th>Album Name</th>
+                <th>Genre</th>
+                <th width="75" align="center">Actions</th>
+            </tr>
+            <c:forEach items="${songDtoList}" var="s">
+                <tr>
+                    <td><my:a href="/songs/${s.getId()}"><c:out value="${s.getTitle()}"/></my:a></td>
+                    <td><c:out value="${s.getMusician().getName()}"/></td>
+                    <td><c:out value="${s.getAlbum().getTitle()}"/></td>
+                    <td><c:out value="${s.getGenre().getName()}"/></td>
+                    <td align="center"><my:a href="/songs/${s.getId()}/delete"  data-confirm="Are you sure to delete this song?">
+                        <img src="<c:url value="/images/delete.png" />" title="Delete" alt="Delete"/>
+                    </my:a>
+                    </td>
+                </tr>
+            </c:forEach>
+        </table>
+    </jsp:attribute>
+</my:pagetemplate>
