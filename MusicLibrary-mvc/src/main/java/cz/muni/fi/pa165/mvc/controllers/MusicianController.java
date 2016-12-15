e0844bb005f0dbc8e40fff10b2e8c4fef9fe1e27
David Pribula <davidpribula@gmail.com>
2016-12-15 12:12:59 +0100

    Adding option for adding new musician

--- MusicLibrary-mvc/src/main/java/cz/muni/fi/pa165/mvc/controllers/MusicianController.java
+++ MusicLibrary-mvc/src/main/java/cz/muni/fi/pa165/mvc/controllers/MusicianController.java
@@ -18,6 +18,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -34,6 +35,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/musicians")
 public class MusicianController {
     final static Logger log = LoggerFactory.getLogger(MusicianController.class);
+    
+    public static final String REDIRECT_MUSICIANS = "redirect:/musicians/";
    
     @Autowired
     private MusicianFacade musicianFacade;
@@ -57,7 +60,19 @@ public class MusicianController {
                 model.addAttribute("musician",musician);
 		return "musicians/details";
 	}
-                
-            
-
+        
+        @RequestMapping(value = "/add", method = RequestMethod.GET)
+	@PreAuthorize("hasAuthority('admin')")
+	public String add(Model model) {
+		model.addAttribute("musicianForm", new MusicianDTO());
+		return "musicians/add";
+	}
+        
+        @RequestMapping(value = "/add", method = RequestMethod.POST)
+	@PreAuthorize("hasAuthority('admin')")
+	public String doAdd(@ModelAttribute("musicianForm") MusicianDTO musician, RedirectAttributes redir) {
+		musicianFacade.createMusician(musician);
+		redir.addFlashAttribute(Alert.SUCCESS, "Successfuly created");
+		return REDIRECT_MUSICIANS;
+	}
 }
--- MusicLibrary-mvc/src/main/webapp/WEB-INF/jsp/musicians/list.jsp
+++ MusicLibrary-mvc/src/main/webapp/WEB-INF/jsp/musicians/list.jsp
@@ -4,10 +4,14 @@
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@ taglib prefix="s" uri="http://www.springframework.org/tags" %>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
 
 <my:pagetemplate title="Musicians">
 <jsp:attribute name="body">
-
+    <sec:authorize access="hasAuthority('admin')">
+            <my:a href="/musicians/add"><button type="button" style="margin-bottom: 20px;" class="btn btn-primary btn-sm pull-right">Add new musician</button></my:a>
+      </sec:authorize>
+        
     <c:forEach items="${musicians}" var="m">
         <h2><c:out value="${m.name}" /></h2>
         <h4><c:out value="Average rating of this musician is ${m.avgAlbumRating} stars." /></h4>
